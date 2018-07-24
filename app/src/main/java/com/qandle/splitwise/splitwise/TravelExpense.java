67c5f1fd92a6f65e28519a0a47fa976bc735b37d
Tanya Goel <goeltanya01@gmail.com>
2018-07-24 17:46:28 +0530

    final

--- app/src/main/java/com/qandle/splitwise/splitwise/TravelExpense.java
+++ app/src/main/java/com/qandle/splitwise/splitwise/TravelExpense.java
@@ -6,7 +6,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
@@ -17,6 +19,8 @@ public class TravelExpense extends AppCompatActivity {
     DatabaseHandler db;
     ListView lv;
     EditText des;
+    Button but1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,20 +30,38 @@ public class TravelExpense extends AppCompatActivity {
         db = new DatabaseHandler(this);
 
         lv = (ListView) findViewById(R.id.list_view);
+//        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//
+//            }
+//        });
         des = (EditText) findViewById(R.id.e1);
-
         loadData();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        but1=findViewById(R.id.button1);
+        but1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity( new Intent (TravelExpense.this,FinalTransaction.class));
-//                startActivityForResult(new Intent(TravelExpense.this,AddTravel.class),101);
+                Intent but1=new Intent(TravelExpense.this,AllNames.class);
+                startActivity(but1);
+
             }
         });
 
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent but2=new Intent(TravelExpense.this,FinalTransaction.class);
+                startActivity(but2);
+
+            }
+        });
+
+
+
     }
+
 
     private void loadData()
     {
@@ -48,6 +70,7 @@ public class TravelExpense extends AppCompatActivity {
 
         ArrayAdapter listAdapter = new ArrayAdapter<Bills>(this, R.layout.names_row, contacts);
         lv.setAdapter(listAdapter);
+
     }
 
     @Override
